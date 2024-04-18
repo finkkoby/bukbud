095a9f9486a447bcf0ed6c85db45dad3765d2d6a
Koby Fink <finkkoby@icloud.com>
2024-04-18 15:32:01 -0400

    users seed data

--- server/models.py
+++ server/models.py
@@ -36,7 +36,7 @@ class User(db.Model, SerializerMixin):
     
     @password_hash.setter
     def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(password).encode('utf-8')
+        password_hash = bcrypt.generate_password_hash(password)
         self._password_hash = password_hash.decode('utf-8')
 
     # Other methods
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,35 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+        fake = Faker()
+        print("Deleting tables...")
+        User.query.delete()
+        
         print("Starting seed...")
         # Seed code goes here!
+
+        print("Creating users...")
+        users = []
+        usernames = []
+        for i in range(25):
+            username = fake.user_name()
+            while username in usernames:
+                username = fake.user_name()
+            
+            usernames.append(username)
+            age = fake.pyint(18, 100)
+
+            user = User(username=username, age=age)
+
+            user.password_hash = user.username + 'password'
+
+            users.append(user)
+
+        db.session.add_all(users)
+        db.session.commit()
+        print("Complete.")

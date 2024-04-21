da0493bfb08555d2296fe41d31e5b12f3661ce3f
Koby Fink <finkkoby@icloud.com>
2024-04-21 17:01:08 -0400

    seed data for books, reviews, authors

--- server/models.py
+++ server/models.py
@@ -18,9 +18,12 @@ class User(db.Model, SerializerMixin):
     age = db.Column(db.Integer)
 
     # Relationships
-
+    reviews = db.relationship("Review", back_populates="user")
+    books = association_proxy("reviews", "book",
+                              creator=lambda book: Review(book=book))
 
     # Serialize Rules
+    serialize_rules = ('-reviews.user',)
 
 
     # Validations
@@ -42,3 +45,71 @@ class User(db.Model, SerializerMixin):
     # Other methods
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+    
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    # Database Schema
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    comment = db.Column(db.String)
+
+    # Relationships
+    book = db.relationship("Book", back_populates="reviews")
+    user = db.relationship("User", back_populates="reviews")
+
+    # Serialize Rules
+    serialize_rules = ('-book.reviews', '-user.reviews')
+
+
+    # Validations
+    @validates('rating')
+    def validate_rating(self, key, value):
+        if not 0.0 <= value <= 5.0:
+            raise ValueError(f'Rating must be between 0.0 and 5.0')
+        return value
+
+    # Other methods
+
+class Book(db.Model, SerializerMixin):
+    __tablename__ = 'books'
+
+     # Database Schema
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    length = db.Column(db.Integer, nullable=False)
+    genre = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False)
+
+    # Relationships
+    reviews = db.relationship("Review", back_populates="book")
+    users = association_proxy("reviews", "user",
+                              creator=lambda user: Review(user=user))
+    author = db.relationship("Author", back_populates="books")
+
+    # Serialize Rules
+    serialize_rules = ('-reviews.book', '-author.books')
+
+    # Validations
+
+
+    # Other methods
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = 'authors'
+
+    # Database Schema
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer)
+    
+    # Relationships
+    books = db.relationship("Book", back_populates="author")
+
+    # Serialize Rules
+    serialize_rules = ('-books.author')
+
+
+    # Other methods
--- server/seed.py
+++ server/seed.py
@@ -8,7 +8,7 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db, User
+from models import db, User, Author, Book, Review
 
 if __name__ == '__main__':
     fake = Faker()
@@ -39,7 +39,38 @@ if __name__ == '__main__':
             user.password_hash = user.username + 'password'
 
             users.append(user)
-
         db.session.add_all(users)
+
+        print("Creating authors...")
+        authors = []
+        for i in range(25):
+            name = fake.name()
+            age = fake.pyint(18, 100)
+            author = Author(name=name, age=age)
+            authors.append(author)
+        db.session.add_all(authors)
+
+
+        print("Creating books...")
+        books = []
+        genres = ["Fiction", "Fantasy", "Science Fiction", "Mystery", "Romance", "Historical Fiction", "Non-Fiction", "Thriller", "Graphic Novel"]
+        for i in range(25):
+            title = fake.sentence().title()
+            author = authors[randint(0, 24)]
+            length = randint(100, 1000)
+            genre = rc(genres)
+            books.append(Book(title=title, author=author, length=length, genre=genre))
+        db.session.add_all(books)
+
+        print("Creating reviews...")
+        reviews = []
+        for i in range(25):
+            user = users[randint(0, 24)]
+            book = books[randint(0, 24)]
+            rating = fake.pyfloat(left_digits=1, right_digits=1, min_value=0, max_value=5)
+            comment = fake.paragraph()
+            reviews.append(Review(user=user, book=book, rating=rating, comment=comment))
+        db.session.add_all(reviews)
+
         db.session.commit()
         print("Complete.")
